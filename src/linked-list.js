b993bf36db6de5b80e474196dd4c7c25c07b894b
ToddH <tdhale@verisk.com>
2019-04-04 17:12:32 -0600

    reformat

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,79 +1,79 @@
-
 class ListNode {
-    constructor(value) {
-        this.nextNode = undefined;
-        this.value = value;
-    }
+  constructor(value) {
+    this.nextNode = undefined;
+    this.value = value;
+  }
 }
 
 class LinkedList {
+  // Array is used to implement stack
+  constructor() {
+    this.head = undefined;
+    this.count = 0; // keep track of count
+  }
 
-    // Array is used to implement stack 
-    constructor() 
-    { 
-        this.head = undefined;
-        this.count = 0; // keep track of count
-    } 
-  
-    // Functions to be implemented 
+  // Functions to be implemented
 
-    insert(value) {
-        // insert at beginning of list
-        var newNode = new ListNode(value);
-        newNode.nextNode = this.head; // point our new node at the current head
-        this.head = newNode; // update head to point at our new node
+  insert(value) {
+    // insert at beginning of list
+    var newNode = new ListNode(value);
+    newNode.nextNode = this.head; // point our new node at the current head
+    this.head = newNode; // update head to point at our new node
 
-        // increment count
-        this.count++;
+    // increment count
+    this.count++;
+  }
+
+  remove(value) {
+    if (this.size() === 0) {
+      // if the list is empty
+      return false;
+    } else if (this.head.value === value) {
+      // if our value is first in the list
+      this.head = this.head.nextNode;
+      this.count--;
+      return true;
+    } else {
+      // if our value isn't first in the list, look at all of the other nodes
+      let previousNode = this.head;
+      while (previousNode.nextNode && previousNode.nextNode.value !== value) {
+        previousNode = previousNode.nextNode;
+      }
+      if (previousNode.nextNode && previousNode.nextNode.value === value) {
+        previousNode.nextNode = previousNode.nextNode.nextNode;
+        this.count--;
+        return true;
+      }
     }
+    return false;
+  }
 
-    remove(value) {
-        if (this.size() === 0) { // if the list is empty
-            return false;
-        } else if (this.head.value === value) { // if our value is first in the list
-            this.head = this.head.nextNode;
-            this.count--;
-            return true;
-        } else { // if our value isn't first in the list, look at all of the other nodes
-            let previousNode = this.head;
-            while (previousNode.nextNode && previousNode.nextNode.value !== value) {
-                previousNode = previousNode.nextNode;
-            }
-            if (previousNode.nextNode && previousNode.nextNode.value === value) {
-                previousNode.nextNode = previousNode.nextNode.nextNode;
-                this.count--;
-                return true;
-            }
-        }
-        return false;
+  contains(value) {
+    // find value and return true or false
+    let currentNode = this.head;
+    while (currentNode !== undefined) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.nextNode;
     }
+    return false;
+  }
 
-    contains(value) {
-        // find value and return true or false
-        let currentNode = this.head;
-        while (currentNode !== undefined) {
-            if (currentNode.value === value) {
-                return true;
-            }
-            currentNode = currentNode.nextNode;
-        }
-        return false;
-    }
+  size() {
+    return this.count;
+  }
 
-    size() {
-        return this.count;
+  toString() {
+    // return list as a comma-delimited string of all values
+    let output = "";
+    let currentNode = this.head;
+    while (currentNode !== undefined) {
+      output += JSON.stringify(currentNode) + "\n";
+      currentNode = currentNode.nextNode;
     }
-
-    toString() {
-        // return list as a comma-delimited string of all values
-        let output = "";
-        let currentNode = this.head;
-        while (currentNode !== undefined) {
-            output += JSON.stringify(currentNode) + "\n";
-            currentNode = currentNode.nextNode;
-        }
-        return output;
-    }
+    return output;
+  }
 }
 
 module.exports.LinkedList = LinkedList;
